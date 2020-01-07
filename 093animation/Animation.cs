023ce53526f5f43e5e65c3922f45746f8eb67962
Eliáš Cizl <cizl.elias@gmail.com>
2020-01-07 10:34:17 +0100

    Animation - Sierpi and rectangles

--- 093animation/Animation.cs
+++ 093animation/Animation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using LineCanvas;
 using MathSupport;
@@ -7,43 +8,55 @@ using Utilities;
 
 namespace _093animation
 {
+ 
   public class Animation
   {
+    private static double screenWidth;
+    private static double screenHeight;
+
     /// <summary>
     /// Form data initialization.
     /// </summary>
     /// <param name="name">Your first-name and last-name.</param>
-    /// <param name="wid">Image width in pixels.</param>
-    /// <param name="hei">Image height in pixels.</param>
+    /// <param name="screenWidth">Image width in pixels.</param>
+    /// <param name="screenHeight">Image height in pixels.</param>
     /// <param name="from">Animation start in seconds.</param>
     /// <param name="to">Animation end in seconds.</param>
     /// <param name="fps">Frames-per-seconds.</param>
     /// <param name="param">Optional text to initialize the form's text-field.</param>
     /// <param name="tooltip">Optional tooltip = param help.</param>
-    public static void InitParams (out string name, out int wid, out int hei, out double from, out double to, out double fps, out string param, out string tooltip)
+    public static void InitParams (out string name, out int screenWidth, out int screenHeight, out double from, out double to, out double fps, out string param, out string tooltip)
     {
       // {{
 
       // Put your name here.
-      name = "Josef Pelikán";
+      name = "Eliáš Cizl";
 
       // Image size in pixels.
-      wid = 640;
-      hei = 480;
+      screenWidth = 1920;
+      screenHeight = 1080;
 
       // Animation.
       from = 0.0;
-      to   = 10.0;
-      fps  = 25.0;
+      to   = 0.5;
+      fps  = 60.0;
 
       // Specific animation params.
-      param = "width=1.0,anti=true,objects=100,hatches=12,prob=0.95";
+      param = "width=1.0,anti=true,rectangles=10,lined=2";
 
       // Tooltip = help.
-      tooltip = "width=<double>, anti[=<bool>], objects=<int>, hatches=<int>, prob=<double>";
+      tooltip = "width=<double>, anti[=<bool>], rectangles=<int>, lined=<int>";
 
       // }}
     }
+
+    private static double start;
+    private static double end;
+    private static double deltaTime;
+    private static float penWidth;
+    private static bool antialias;
+    private static int rectanglesCount;
+    private static int lined;
 
     /// <summary>
     /// Global initialization. Called before each animation batch
@@ -57,210 +70,134 @@ namespace _093animation
     /// <param name="param">Optional string parameter from the form.</param>
     public static void InitAnimation (int width, int height, double start, double end, double fps, string param)
     {
-      // {{ TODO: put your init code here
+      Animation.start = start;
+      screenWidth = width;
+      screenHeight = height;
+      deltaTime = 1.0 / fps;
+      Animation.end = end + deltaTime; // updating end time
+      deltaTime = 1.0 / (fps + 1); // updating deltaTime
 
-      // }}
+
+      // input params:
+      penWidth = 1.0f;   // pen width
+      antialias = false;  // use anti-aliasing?
+      rectanglesCount = 10;
+      lined = 2;
+
+      Dictionary<string, string> p = Util.ParseKeyValueList(param);
+      if (p.Count > 0)
+      {
+        if (Util.TryParse(p, "width", ref penWidth) && penWidth < 0.0f)
+          penWidth = 1.0f;
+
+        Util.TryParse(p, "anti", ref antialias);
+
+        if (Util.TryParse(p, "rectangles", ref rectanglesCount) && rectanglesCount < 1)
+          rectanglesCount = 10;
+
+        if (Util.TryParse(p, "lined", ref lined) && lined < 1)
+          lined = 2;
+      }
+
+      
     }
 
     /// <summary>
     /// Draw single animation frame.
     /// Has to be re-entrant!
     /// </summary>
-    /// <param name="c">Canvas to draw to.</param>
+    /// <param name="canvas">Canvas to draw to.</param>
     /// <param name="time">Current time in seconds.</param>
     /// <param name="start">Start time (t0)</param>
     /// <param name="end">End time (for animation length normalization).</param>
     /// <param name="param">Optional string parameter from the form.</param>
-    public static void DrawFrame (Canvas c, double time, double start, double end, string param)
+    public static void DrawFrame (Canvas canvas, double time, double start, double end, string param)
     {
-      // {{ TODO: put your drawing code here
+      canvas.SetPenWidth(penWidth);
+      canvas.SetAntiAlias(antialias);
 
-      double timeNorm = Arith.Clamp((time - start) / (end - start), 0.0, 1.0);
+      // removing bias
+      start = Animation.start;
+      end = Animation.end;
 
-      // input params:
-      float penWidth = 1.0f;   // pen width
-      bool antialias = false;  // use anti-aliasing?
-      int objects    = 100;    // number of randomly generated objects (squares, stars, Brownian particles)
-      int hatches    = 12;     // number of hatch-lines for the squares
-      double prob    = 0.95;   // continue-probability for the Brownian motion simulator
+      double timeNorm = (time - start) / (end - start);
 
-      Dictionary<string, string> p = Util.ParseKeyValueList(param);
-      if (p.Count > 0)
+      double change = Math.Pow(2, timeNorm);
+      double rightChange = change * screenWidth / 2;
+      double upChange = change * screenHeight;
+      //sierpinsky part
+      drawSierpinsky(canvas, 10, screenWidth / 2 + rightChange / 2, screenHeight - upChange, screenWidth / 2, screenHeight, screenWidth / 2 + rightChange, screenHeight);
+
+      timeNorm *= lined;
+
+      for (int i = 0; i < rectanglesCount; i++)
       {
-        // with=<line-width>
-        if (Util.TryParse(p, "width", ref penWidth))
+        // i / rectanglesCount -> (i + 1) / rectanglesCount
+        double coefficient1 = (i + timeNorm) / rectanglesCount;
+        double distance1 = Math.Pow(2, coefficient1) - 1;//1 / Math.Pow(2, 10 * (1 - coefficient1));
+        DrawCenterRectangle(canvas, distance1);
+
+        double coefficient2 = (i + 1 + timeNorm) / rectanglesCount;
+        double distance2 = Math.Pow(2, coefficient2) - 1;//1 / Math.Pow(2, 10 * (1 - coefficient2));
+        if (i % lined == 0)
         {
-          if (penWidth < 0.0f)
-            penWidth = 0.0f;
-        }
-
-        // anti[=<bool>]
-        Util.TryParse(p, "anti", ref antialias);
-
-        // squares=<number>
-        if (Util.TryParse(p, "objects", ref objects) &&
-            objects < 0)
-          objects = 0;
-
-        // hatches=<number>
-        if (Util.TryParse(p, "hatches", ref hatches) &&
-            hatches < 1)
-          hatches = 1;
-
-        // prob=<probability>
-        if (Util.TryParse(p, "prob", ref prob) &&
-            prob > 0.999)
-          prob = 0.999;
-      }
-
-      int wq = c.Width / 4;
-      int hq = c.Height / 4;
-      int wh = wq + wq;
-      int hh = hq + hq;
-      int minh = Math.Min(wh, hh);
-      double t;
-      int i, j;
-      double cx, cy, angle, x, y;
-
-      c.Clear(Color.Black);
-
-      // 1st quadrant - star.
-      c.SetPenWidth(penWidth);
-      c.SetAntiAlias(antialias);
-
-      const int MAX_LINES = 30;
-      for (i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES)
-      {
-        c.SetColor(Color.FromArgb(i * 255 / MAX_LINES, 255, 255 - i * 255 / MAX_LINES)); // [0,255,255] -> [255,255,0]
-        c.Line(t * wh, 0, wh - t * wh, hh);
-      }
-      for (i = 0, t = 0.0; i < MAX_LINES; i++, t += 1.0 / MAX_LINES)
-      {
-        c.SetColor(Color.FromArgb(255, 255 - i * 255 / MAX_LINES, i * 255 / MAX_LINES)); // [255,255,0] -> [255,0,255]
-        c.Line(0, hh - t * hh, wh, t * hh);
-      }
-
-      // 2nd quadrant - random hatched squares.
-      double size = minh / 10.0;
-      double padding = size * Math.Sqrt(0.5);
-      c.SetColor(Color.LemonChiffon);
-      c.SetPenWidth(1.0f);
-      Random r = new Random(12);
-
-      for (i = 0; i < objects; i++)
-      {
-        do
-          cx = r.NextDouble() * wh;
-        while (cx < padding ||
-               cx > wh - padding);
-
-        c.SetAntiAlias(cx > wq);
-        cx += wh;
-
-        do
-          cy = r.NextDouble() * hh;
-        while (cy < padding ||
-               cy > hh - padding);
-
-        angle = (r.NextDouble() + timeNorm) * Math.PI;
-
-        double dirx = Math.Sin(angle) * size * 0.5;
-        double diry = Math.Cos(angle) * size * 0.5;
-        cx -= dirx - diry;
-        cy -= diry + dirx;
-        double dx = -diry * 2.0 / hatches;
-        double dy = dirx * 2.0 / hatches;
-        double linx = dirx + dirx;
-        double liny = diry + diry;
-
-        for (j = 0; j++ < hatches; cx += dx, cy += dy)
-          c.Line(cx, cy, cx + linx, cy + liny);
-      }
-
-      // 3rd quadrant - random stars.
-      c.SetColor(Color.LightCoral);
-      c.SetPenWidth(penWidth);
-      size = minh / 16.0;
-      padding = size;
-      const int MAX_SIDES = 30;
-      List<PointF> v = new List<PointF>(MAX_SIDES + 1);
-
-      for (i = 0; i < objects; i++)
-      {
-        do
-          cx = r.NextDouble() * wh;
-        while (cx < padding ||
-               cx > wh - padding);
-
-        c.SetAntiAlias(cx > wq);
-
-        do
-          cy = r.NextDouble() * hh;
-        while (cy < padding ||
-               cy > hh - padding);
-
-        cy += hh;
-
-        int sides = r.Next(3, MAX_SIDES);
-        double dAngle = Math.PI * 2.0 / sides;
-
-        v.Clear();
-        angle = 0.0;
-
-        for (j = 0; j++ < sides; angle += dAngle)
-        {
-          double rad = size * (0.1 + 0.9 * r.NextDouble());
-          x = cx + rad * Math.Sin(angle);
-          y = cy + rad * Math.Cos(angle);
-          v.Add(new PointF((float)x, (float)y));
-        }
-        v.Add(v[0]);
-        c.PolyLine(v);
-      }
-
-      // 4th quadrant - Brownian motion.
-      c.SetPenWidth(penWidth);
-      c.SetAntiAlias(true);
-      size = minh / 10.0;
-      padding = size;
-
-      for (i = 0; i < objects; i++)
-      {
-        do
-          x = r.NextDouble() * wh;
-        while (x < padding ||
-               x > wh - padding);
-
-        do
-          y = r.NextDouble() * hh;
-        while (y < padding ||
-               y > hh - padding);
-
-        c.SetColor(Color.FromArgb(127 + r.Next(0, 128),
-                                  127 + r.Next(0, 128),
-                                  127 + r.Next(0, 128)));
-
-        for (j = 0; j++ < 1000;)
-        {
-          angle = r.NextDouble() * Math.PI * 2.0;
-          double rad = size * r.NextDouble();
-          cx = x + rad * Math.Sin(angle);
-          cy = y + rad * Math.Cos(angle);
-          if (cx < 0.0 || cx > wh ||
-              cy < 0.0 || cy > hh)
-            break;
-
-          if (j < 70.0 * timeNorm)
-            c.Line(x + wh, y + hh, cx + wh, cy + hh);
-
-          x = cx;
-          y = cy;
-          if (r.NextDouble() > prob)
-            break;
+          DrawCenterConnectingLines(canvas, distance1, distance2);
         }
       }
+    }
 
-      // }}
+    private static void DrawCenterConnectingLines (Canvas canvas, double distance1, double distance2)
+    {
+      DrawConnectingLines(canvas, screenWidth / 4, screenHeight / 2, distance1, distance2);
+    }
+
+    private static void DrawConnectingLines (Canvas canvas, double centerX, double centerY, double distance1, double distance2)
+    {
+      double horizontalDistance1 = (screenWidth / 4) * distance1;
+      double verticalDistance1 = (screenHeight / 2) * distance1;
+      double horizontalDistance2 = (screenWidth / 4) * distance2;
+      double verticalDistance2 = (screenHeight / 2) * distance2;
+      canvas.Line(centerX + horizontalDistance1, centerY - verticalDistance1, centerX + horizontalDistance2, centerY - verticalDistance2); // top right
+      canvas.Line(centerX + horizontalDistance1, centerY + verticalDistance1, centerX + horizontalDistance2, centerY + verticalDistance2); // bottom right
+      canvas.Line(centerX - horizontalDistance1, centerY + verticalDistance1, centerX - horizontalDistance2, centerY + verticalDistance2); // bottom left
+      canvas.Line(centerX - horizontalDistance1, centerY - verticalDistance1, centerX - horizontalDistance2, centerY - verticalDistance2); // top left
+    }
+
+    private static void DrawCenterRectangle (Canvas canvas, double distanceFromCenterNormalized)
+    {
+      DrawRectangle(canvas, screenWidth / 4, screenHeight / 2, distanceFromCenterNormalized);
+    }
+
+    private static void DrawRectangle (Canvas canvas, double centerX, double centerY, double distanceFromCenterNormalized)
+    {
+      double horizontalDistance = (screenWidth / 4) * distanceFromCenterNormalized;
+      double verticalDistance = (screenHeight / 2) * distanceFromCenterNormalized;
+      if (horizontalDistance <= screenWidth / 4)
+        canvas.Line(centerX + horizontalDistance, centerY - verticalDistance, centerX + horizontalDistance, centerY + verticalDistance); // right
+      canvas.Line(centerX + horizontalDistance, centerY + verticalDistance, centerX - horizontalDistance, centerY + verticalDistance); // bottom
+      canvas.Line(centerX - horizontalDistance, centerY + verticalDistance, centerX - horizontalDistance, centerY - verticalDistance); // left
+      canvas.Line(centerX - horizontalDistance, centerY - verticalDistance, centerX + horizontalDistance, centerY - verticalDistance); // top
+    }
+
+    private static void drawSierpinsky (Canvas canvas, int n, double x1, double y1, double x2, double y2, double x3, double y3)
+    {
+      Point[] triangle = new Point[] { new Point((int)x1, (int)y1), new Point((int)x2, (int)y2), new Point((int)x3, (int)y3) };
+      double leftX = (x1 + x2) / 2;
+      double leftY = (y1 + y2) / 2;
+      double rightX = (x1 + x3) / 2;
+      double rightY = (y1 + y3) / 2;
+      double bottomX = (x2 + x3) / 2;
+      double bottomY = (y2 + y3) / 2;
+      if (n > 0)
+      {
+        canvas.Line(triangle[0].X, triangle[0].Y, triangle[1].X, triangle[1].Y);
+        canvas.Line(triangle[0].X, triangle[0].Y, triangle[2].X, triangle[2].Y);
+        canvas.Line(triangle[1].X, triangle[1].Y, triangle[2].X, triangle[2].Y);
+        n--;
+        drawSierpinsky(canvas, n, leftX, leftY, x2, y2, bottomX, bottomY);
+        drawSierpinsky(canvas, n, x1, y1, leftX, leftY, rightX, rightY);
+        drawSierpinsky(canvas, n, rightX, rightY, bottomX, bottomY, x3, y3);
+      }
     }
   }
 }
